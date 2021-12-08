9f5427622ddfb7bd92e8e3eaeda8afdbc2ca6afd
Eduardo Tapia <tapiaes.eduardo@gmail.com>
2021-12-07 21:48:06 -0700

    Validar serial por clave publica y privada

---
+++ CryptoLibrary/AES.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoLibrary
+{
+    public class AES
+    {
+        private static string _privateKey;
+        private static string _publicKey;
+        private const int _aesKeySize = 256;
+
+        public void SetPublicKey(string key) 
+        {
+            _publicKey = key;
+        }
+
+        public void SetPrivateKey(string key)
+        {
+            _privateKey = key;
+        }
+
+        public string Encrypt(string data)
+        {
+            byte[] init_vector_bytes = Encoding.UTF8.GetBytes(_privateKey);
+            byte[] plain_text_bytes = Encoding.UTF8.GetBytes(data);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(_publicKey, null);
+            byte[] key_bytes = password.GetBytes(_aesKeySize / 8);
+            RijndaelManaged symmetric_key = new RijndaelManaged();
+            symmetric_key.Mode = CipherMode.CBC;
+            ICryptoTransform encryptor = symmetric_key.CreateEncryptor(key_bytes, init_vector_bytes);
+            MemoryStream memory_stream = new MemoryStream();
+            CryptoStream crypto_Stream = new CryptoStream(memory_stream, encryptor, CryptoStreamMode.Write);
+            crypto_Stream.Write(plain_text_bytes, 0, plain_text_bytes.Length);
+            crypto_Stream.FlushFinalBlock();
+            byte[] encrypted = memory_stream.ToArray();
+            memory_stream.Close();
+            crypto_Stream.Close();
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public string Decrypt(string data)
+        {
+            byte[] init_vector_bytes = Encoding.ASCII.GetBytes(_privateKey);
+            byte[] decrypted_text = Convert.FromBase64String(data);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(_publicKey, null);
+            byte[] key_bytes = password.GetBytes(_aesKeySize / 8);
+            RijndaelManaged symmetric_key = new RijndaelManaged();
+            symmetric_key.Mode = CipherMode.CBC;
+            ICryptoTransform decryptor = symmetric_key.CreateDecryptor(key_bytes, init_vector_bytes);
+            MemoryStream memory_stream = new MemoryStream(decrypted_text);
+            CryptoStream crypto_stream = new CryptoStream(memory_stream, decryptor, CryptoStreamMode.Read);
+            byte[] plain_text_bytes = new byte[decrypted_text.Length];
+            int decrypted_byte_count = crypto_stream.Read(plain_text_bytes, 0, plain_text_bytes.Length);
+            memory_stream.Close();
+            crypto_stream.Close();
+            return Encoding.UTF8.GetString(plain_text_bytes, 0, decrypted_byte_count);
+        }
+
+    }
+}
--- SerialkeysGeneratorSystem/MainWindow.xaml.cs
+++ SerialkeysGeneratorSystem/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using KeysLibrary.Services;
+﻿using CryptoLibrary;
+using KeysLibrary.Services;
+using SerialkeysGeneratorSystem.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -38,12 +40,55 @@ namespace SerialkeysGeneratorSystem
             {
                 file.WriteLine(public_key);
             }
+
+            TxtPublicKey.Text = public_key;
             var p = new Process();
             p.StartInfo = new ProcessStartInfo(path + "\\clave.txt")
             {
                 UseShellExecute = true
             };
             p.Start();
+        }
+
+        private void BtnGenerateSerial_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var public_key = TxtPublicKey.Text;
+                if (public_key == string.Empty)
+                    throw new Exception("Debe generar la llave publica primero. ");
+                AES aes_crypto = new AES();
+                aes_crypto.SetPrivateKey("ghdy65et73564gdt");
+                aes_crypto.SetPublicKey(public_key);
+
+                DateTime time_now = AppHelper.GetNetworkTime();
+
+                var encrypted_data = aes_crypto.Encrypt("Hola mundo!");
+                TxtSerial.Text = encrypted_data;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnValidateSerial_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IKeysServices keyService = new KeysServices();
+                var public_key = keyService.GeneratePublicKey();
+                AES aes_crypto = new AES();
+                aes_crypto.SetPrivateKey("ghdy65et73564gdt");
+                aes_crypto.SetPublicKey(public_key);
+                var serial = TxtValidateSerial.Text;
+                var decrypted_data = aes_crypto.Decrypt(serial);
+                MessageBox.Show("Serial valido!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Serial no valido!");
+            }
         }
     }
 }
